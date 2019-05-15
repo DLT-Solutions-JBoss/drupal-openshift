03be9cd462c1463b237096d948e24002ce76d71d
Rick Stewart <rick.stewart@dlt.com>
2019-05-15 17:43:22 -0400

    Update init_settings.sh

--- init_settings.sh
+++ init_settings.sh
@@ -8,3 +8,13 @@ else
 fi
 
 cp ./sites-cp/default/*settings.php ./sites/default/.
+
+if [ -d "./sites/default/config/sync" ]
+then
+	echo "sites default config directory  exists!"
+else
+	mkdir "./sites/default/config"
+  mkdir "./sites/default/config/sync"
+fi
+
+cp ./sites-cp/default/config/sync/* ./sites/default/config/sync/.
