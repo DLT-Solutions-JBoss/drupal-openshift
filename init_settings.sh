431dc613e3b0a80c6166f0165d9a17d49477f43b
Rick Stewart <rick.stewart@dlt.com>
2019-05-17 09:26:27 -0400

    Removing config/sync files and init copy

--- init_settings.sh
+++ init_settings.sh
@@ -8,11 +8,11 @@ else
   cp ./sites-cp/default/*settings.php ./sites/default/.
 fi
 
-if [ -d "./sites/default/config/sync" ]
-then
-	echo "sites default config directory  exists!"
-else
-	mkdir "./sites/default/config"
-  mkdir "./sites/default/config/sync"
-  cp ./sites-cp/default/config/sync/* ./sites/default/config/sync/.
-fi
+#if [ -d "./sites/default/config/sync" ]
+#then
+#  echo "sites default config directory  exists!"
+#else
+#  mkdir "./sites/default/config"
+#  mkdir "./sites/default/config/sync"
+#  cp ./sites-cp/default/config/sync/* ./sites/default/config/sync/.
+#fi
