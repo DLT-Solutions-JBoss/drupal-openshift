e1ea343ad42b26b75f06e13c30e24c6ed09ab40d
Rick Stewart <rick.stewart@dlt.com>
2019-05-23 10:09:11 -0400

    used composer to install modules/themes

--- init_settings.sh
+++ init_settings.sh
@@ -5,7 +5,7 @@ then
 	echo "sites default directory  exists!"
 else
 	mkdir "./sites/default"
-  cp ./sites-cp/* ./sites/.
+  cp -r ./sites-cp/* ./sites/.
 fi
 
 #if [ -d "./sites/default/config/sync" ]
